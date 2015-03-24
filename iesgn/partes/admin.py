bfe524d7139d4f8903f4a035e70aaaed045b4322
josedom24 <josedom24@gmail.com>
2015-03-24 20:55:20 +0100

    Prueba amdin.py

--- iesgn/partes/admin.py
+++ iesgn/partes/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from partes.models import Cursos,Alumnos,Profesores,Departamentos,Partes
-
+from django.utils.html import format_html
 class AlumnosAdmin(admin.ModelAdmin):
     date_hierarchy = 'Fecha_nacimiento'
     actions_selection_counter=False
     list_filter = ['Unidad','Localidad']
-    
+    list_display = ["Nombre",'DNI',"Unidad","Telefono1","enlace"]
+    list_display_links = ["Nombre",'DNI']
+    list_editable = ["Telefono1"]
+    search_fields = ['Nombre','DNI']
+
+    def enlace(self,obj):
+   		return format_html('<a href="#">Enlace</a>')
+
 admin.site.register(Cursos)
 admin.site.register(Alumnos,AlumnosAdmin)
 admin.site.register(Profesores)
